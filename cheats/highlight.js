d044905bf7bd7a75111b25de64ac1a0c2bb88af3
Soldy <4786022+Soldy@users.noreply.github.com>
2021-01-03 11:51:35 +0000

    to call
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- cheats/highlight.js
+++ cheats/highlight.js
@@ -1,24 +1,26 @@
 
-const highLigthBase = function(){
-    this.highlight = function(style, list, element, before, after){
-         list = listMidifier(list);
-         highligth(list, element, before, after);
-    }
-    const listMidifier = function(list){
+const highLigthBase = function(){ // not a miss spell  
+    this.highlight = function(list, element, before, after){
+        list = listMidifier(list);
+        highligth(list, element, before, after);
+    };
+    const listMidifier = function(list){ // have to rethink need more rule
         let out = [];
         for(const i of list){
             out.push(' '+i+',');
             out.push(' '+i+' ');
         }
-    }
+    };
     const highligth = function(list, element, before, after){
         const html = element.innerHTML;
-        element.innerHTML =  html.replace(
-            (new RegExp(list.join("|"), "gi")), 
+        element.innerHTML =  html.replace( // the replace is fast but noit effective so 
+            (new RegExp(list.join('|'), 'gi')), 
             function(x){
                 return before+x+after; 
             }
         );
-    }
-}
+    };
+};
 
+
+const highLigth = new highLigthBase();
