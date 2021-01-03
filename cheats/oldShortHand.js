339fe5bab9972a47cb248bae56a75cae7b7410a8
Soldy <4786022+Soldy@users.noreply.github.com>
2021-01-03 11:46:40 +0000

    shine baby shine
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- cheats/oldShortHand.js
+++ cheats/oldShortHand.js
@@ -4,17 +4,17 @@ const eCIR = function(n){ // element change id Remove
     }catch(e){
         GM.notification(n, 'eCIR');
     }
-}
+};
 const eCC = function(){ // element change class 
 
-}
+};
 const eCCR = function(n){ // element change class Remove
     try{
         document.getElementsByClassName(n)[0].remove();
     }catch(e){
-         GM.notification(n, 'eCCR');
-    };
-}
+        GM.notification(n, 'eCCR');
+    }
+};
 const eCCS = function(n, s){ // element change class Style
     try{
         document.getElementsByClassName(n)[0].setAttribute(
@@ -22,12 +22,12 @@ const eCCS = function(n, s){ // element change class Style
             s
         );          
     }catch(e){
-         GM.notification(n, 'eCCS');
-    };
-}
+        GM.notification(n, 'eCCS');
+    }
+};
 const eCCa = function(){ // element change class 
 
-}
+};
 const eCNS = function(n, s){ // element change class Style
     try{
         document.getElementsByTagName(n)[0].setAttribute(
@@ -35,5 +35,5 @@ const eCNS = function(n, s){ // element change class Style
             s
         );          
     }catch(e){
-    };
-}
+    }
+};
